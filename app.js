b6c62678de65a211153781199e09eb9eee562879
husna saadi <saadiasaadi944@gmail.com>
2024-03-17 09:02:59 +0500

    assignment no 17

---
+++ app.js
@@ -0,0 +1,145 @@
+// chap 14
+
+// q;1
+// var password=+prompt("enter password");
+// if(password.length<=5){
+//     alert("password must be greater than 5");
+// } else{
+//     alert("0k");
+// }
+// q;2
+// if(a===1){if(c==="max"){
+//     alert("ok");
+// }
+// }
+// q;3
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+// }
+// q;4
+// var a=5;
+// var b=5;
+// if(a===b){
+//     if(a<=b){
+//         alert("the condition pass")
+//     }
+// }
+
+
+// chap ;15 (array I)
+
+// q;1
+// Array=[];
+
+// q;2
+// let myArray=["hello world"];
+// console.log(myArray);
+
+// q;3
+// var array=["h","i","j","k"];
+// alert(array[2]);
+
+// q;4
+// var alphabet=["l","m","n","o","p"];
+// var length=alphabet.length
+// alert(length);
+
+// q;5
+// var element=["abcd"];
+// element.push("efgh");
+// alert(element);
+
+
+// chap array(II)
+// q;1
+// ar alpha=["husna"];
+// alert(alpha);
+//  var edit=alpha.push("saadi");
+// alert(edit);
+
+// q;2
+//  var Alphabet=["h","i","j","k"];
+//  Alphabet.pop();
+//  alert(Alphabet)
+
+// q;3
+// var Alphabet=["h","i","j","k"];
+//  Alphabet.push("l","m");
+//  alert(Alphabet)
+
+// chap 16(III)
+
+//q;1
+// var sizes = ["s","m","L","XL","XXL","XXXL"];
+// sizes.shift("s");
+// alert(sizes)
+
+// q;2
+// var sizes = ["s","m","L","XL","XXL","XXXL"];
+// sizes.unshift("small","large","medium");
+// alert(sizes)
+
+// q;3
+// sizes = ["s","m","XL","XXL","XXXL"];
+// sizes.splice(2,0,"L");
+// alert(sizes)
+
+// q;4
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0, 3);
+// alert(regSizes)
+
+// q;5
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+// // Remove "cat", "ox", and "duck" starting from index 1
+// pets.splice(1, 3);
+
+// // Add two elements after "dog"
+// pets.splice(2, 0, "parrot", "rabbit");
+
+// console.log(pets);
+
+// chap loop 
+// q;1
+// for (var i = 0; i < 10; i++) {
+//     console.log("hello world")
+// }
+
+// q;2
+// for (var i = 0; i < 10; i++){
+//     console.log("husna")
+// }
+
+// q;3
+// for (var i=0;i<=4;i++){
+//     console.log("hello")
+// }
+
+// q;4
+// for (var name=0;name<100;name++){
+//     console.log("hello world")
+// }
+
+// q;5
+// for (var i=4;i>1;i--){
+//     console.log("hello world")
+// }
+
+// q;7
+// var flag = true; 
+
+// q;8
+// for (var i = 0; i < pets.length; i++) {
+//     console.log("cat","dog","chick")
+// }
+
+// // q;12
+// var firstArray=["a","b","c","d","f"];
+// var secondArray=[1,2,3,4,5,6];
+// for(var i=0;i<firstArray.length;i++){
+//     for(var j=0;j<secondArray.length;j++){
+//         console.log(firstArray[i]+secondArray[j]);
+//     }
+// }
+
